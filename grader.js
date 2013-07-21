25272feb5ac6adcfd771a380cdb9453b62dfd3b0
Xiangru Chen <cxreinstein@gmail.com>
2013-07-21 19:03:11 +0800

    updated grader.js and checks.json

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var TMP_HTML_FILE = "tmp.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +63,27 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+function checkFile(file, checks) {
+    var checkJson = checkHtmlFile(file, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to web page', null, 'http://secret-badlands-6223.herokuapp.com/')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson, outJson;
+    if (program.url) {
+	rest.get(program.url).on('complete', function(result, response) {
+	    fs.writeFileSync(TMP_HTML_FILE, result);
+	    checkFile(TMP_HTML_FILE, program.checks);
+	});
+    } else {
+	checkFile(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
